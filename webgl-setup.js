12ec59a89245dbe10d52226d2810ea97e66a8f76
Yngvi Þór Sigurjónsson <blitzkopf@gmail.com>
2014-08-26 02:40:21 +0000

    Add map for reference, fix lighting code

--- webgl-setup.js
+++ webgl-setup.js
@@ -1,5 +1,18 @@
 var gl;
 
+	function sphere2Cart(lat,lon,r) {
+		var point = {};
+		var phi = degToRad(lat);
+		var theta = degToRad(90-lon);
+
+		point.x = r*Math.sin(phi)*Math.cos(theta);
+		point.y = r*Math.sin(phi)*Math.sin(theta);
+		point.z = r*Math.cos(phi);
+		return point;
+	}
+
+
+
 function initGL(canvas) {
     try {
         gl = canvas.getContext("experimental-webgl");
@@ -69,11 +82,17 @@ function initShaders() {
     shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
     gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
 
+    shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "aVertexNormal");
+    gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
+
 /*      shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
     gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
 */
 	shaderProgram.uColorAttribute = gl.getUniformLocation(shaderProgram, "uVertexColor");
     //gl.enableVertexAttribArray(shaderProgram.uColorAttribute);
+	shaderProgram.textureCoordAttribute = gl.getAttribLocation(shaderProgram, "aTextureCoord");
+    gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
+
 
     shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
     shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
@@ -121,14 +140,19 @@ function degToRad(degrees) {
 
 
 var icoVertexPositionBuffer;
-var circVertexPositionBuffer;
+var icoVertexTextureCoordBuffer;
+var icoNormalVectorBuffer;
 
-var icoVertexColorBuffer;
+var circVertexPositionBuffer;
+var circVertexTextureCoordBuffer;
+
+var mapVertexPositionBuffer;
+var whiteTexture
 var icoVertexIndexBuffer;
 var vertices;
-var circVert = [];
-var midPointCache = {};
 
+var midPointCache = {};
+;
 function getMiddlePoint(p0,p1) {
 	if(p0 < p1 ) {
 		key = p0+":"+p1;
@@ -161,13 +185,13 @@ function getMiddlePoint(p0,p1) {
 	
 }
 
-function initBuffers() {
+function initIcoSphereBuffer(recursion) {
 
 
 	// http://blog.andreaskahler.com/2009/06/creating-icosphere-mesh-in-code.html
 	//t = 1.6180339887; // (1.0 + Math.Sqrt(5.0)) / 2.0;
-	s = 0.447213595; // 1 / sqrt(5)
-	t = 0.894427191; // 2 / sqrt(5)
+	s = 1 / Math.sqrt(5); 
+	t = 2 / Math.sqrt(5);
 
     vertices = [
         
@@ -216,8 +240,8 @@ function initBuffers() {
 		9, 8, 1,
 
     ];
-	
-	for ( k = 0 ; k < 2 ; k++) {
+	// split the triangles into 4 new ones.
+	for ( k = 0 ; k < recursion ; k++) {
 		var icoVertexIndices2 = [];	
 		for(i=0; i<icoVertexIndices.length ; i+=3) {
 			v0 = icoVertexIndices[i];
@@ -254,25 +278,108 @@ function initBuffers() {
     icoVertexPositionBuffer.itemSize = 3;
     icoVertexPositionBuffer.numItems = vertices.length/3;
 
+	var textureCoords = [];
+	var normalVect = [];
+	for(i=0;i<vertices.length/3;i++) {
+		textureCoords[2*i] = 0;
+		textureCoords[2*i+1] = 0;
+		v1 = vec3.normalize([vertices[3*i],vertices[3*i+1],vertices[3*i+2]]);
+		normalVect[3*i+0]=v1[0];
+		normalVect[3*i+1]=v1[1];
+		normalVect[3*i+2]=v1[2];
+	}
+	icoVertexTextureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, icoVertexTextureCoordBuffer);
+ 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
+    icoVertexTextureCoordBuffer.itemSize = 2;
+    icoVertexTextureCoordBuffer.numItems = textureCoords.length/2;
+
+	icoNormalVectorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER,icoNormalVectorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normalVect), gl.STATIC_DRAW);
+	icoNormalVectorBuffer.itemSize=3;
+	icoNormalVectorBuffer.numItems=normalVect.length/3;
+	
+
     icoVertexIndexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, icoVertexIndexBuffer);
 
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(icoVertexIndices), gl.STATIC_DRAW);
     icoVertexIndexBuffer.itemSize = 1;
     icoVertexIndexBuffer.numItems = icoVertexIndices.length;
+
+	whiteTexture = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, whiteTexture);
+	var whitePixel = new Uint8Array([255, 255, 255, 255]);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, 
+		          gl.RGBA, gl.UNSIGNED_BYTE, whitePixel);
 }
 
-function initCircle(parts) {
+function initCircleBuffer(parts) {
+	var circVert = [];
+	var textureCoords = [];
 	for(i=0 ; i<parts ; i++) {
 		circVert[3*i + 0] = Math.sin(2.0*i*Math.PI/parts);
 		circVert[3*i + 1] = Math.cos(2.0*i*Math.PI/parts);
 		circVert[3*i + 2] = 0;
+		textureCoords[2*i] = 0;
+		textureCoords[2*i+1] = 0;	
 	}
 	circVertexPositionBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER,circVertexPositionBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(circVert),gl.STATIC_DRAW);
 	circVertexPositionBuffer.itemSize = 3;
 	circVertexPositionBuffer.numItems = parts;
+
+	circVertexTextureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, circVertexTextureCoordBuffer);
+ 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
+    circVertexTextureCoordBuffer.itemSize = 2;
+    circVertexTextureCoordBuffer.numItems = textureCoords.length/2;
+}
+
+function initMapBuffer() {
+	/*var depth = 6705;
+	p1 = sphere2Cart(65.5,-18.52,depth);
+	p2 = sphere2Cart(63.8,-18.55,depth);
+	p3 = sphere2Cart(65.45 ,-14.8,depth);
+	p4 = sphere2Cart(63.75,-15.08,depth);
+	vertices = [
+	     p3.x,  p3.y,  p3.z,
+         p4.x,  p4.y,  p4.z,
+         p1.x,  p1.y,  p1.z,
+         p2.x,  p2.y,  p2.z
+	];
+*/
+	mapVertexPositionBuffer = gl.createBuffer();
+	/*gl.bindBuffer(gl.ARRAY_BUFFER,mapVertexPositionBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices),gl.STATIC_DRAW);*/
+	mapVertexPositionBuffer.itemSize = 3;
+	mapVertexPositionBuffer.numItems = 4;
+	mapVertexTextureCoordBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, mapVertexTextureCoordBuffer);
+        var textureCoords = [
+            1.0, 1.0,
+            1.0, 0.0,
+            0.0, 1.0,
+            0.0, 0.0];
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
+    mapVertexTextureCoordBuffer.itemSize = 2;
+    mapVertexTextureCoordBuffer.numItems = 4;
+	var normalVect = [];
+	for(i=0;i<vertices.length/3;i++) {
+		v1 = vec3.normalize([vertices[3*i],vertices[3*i+1],vertices[3*i+2]]);
+		normalVect[3*i+0]=v1[0];
+		normalVect[3*i+1]=v1[1];
+		normalVect[3*i+2]=v1[2];
+	}
+	mapNormalVectorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER,mapNormalVectorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normalVect), gl.STATIC_DRAW);
+	mapNormalVectorBuffer.itemSize=3;
+	mapNormalVectorBuffer.numItems=normalVect.length/3;
+
+
 }
 function setLighting() {
 	gl.uniform1i(shaderProgram.useLightingUniform, 1);
@@ -307,12 +414,15 @@ function drawSphere(pos,sz,color)
 	gl.bindBuffer(gl.ARRAY_BUFFER, icoVertexPositionBuffer);
 	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, icoVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
+	gl.bindBuffer(gl.ARRAY_BUFFER, icoNormalVectorBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, icoNormalVectorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, icoVertexTextureCoordBuffer);
+    gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, icoVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
+		
 	gl.uniform4fv(shaderProgram.uColorAttribute, color);
+	gl.bindTexture(gl.TEXTURE_2D, whiteTexture); 
 
-
-	/*gl.bindBuffer(gl.ARRAY_BUFFER, icoVertexColorBuffer);
-	gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, icoVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	*/
 	setMatrixUniforms();
 
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, icoVertexIndexBuffer);
@@ -330,16 +440,21 @@ function drawEarthWF(pos,radius)
 	mat4.translate(mvMatrix, [pos.x, pos.y, pos.z]);
 	mat4.rotate(mvMatrix,Math.PI/2.0,[0,1,0]);
 	mat4.scale(mvMatrix,[radius,radius,radius]);
+	gl.bindBuffer(gl.ARRAY_BUFFER, circVertexPositionBuffer)
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, circVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, circVertexTextureCoordBuffer);
+	gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, circVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	gl.bindTexture(gl.TEXTURE_2D, whiteTexture); 
+
 	// draw longtitutes
 	for( lon  = 0; lon < 180 ; lon ++) {
 		mvPushMatrix();
 
 		mat4.rotate(mvMatrix,lon*Math.PI/180,[1,0,0]);
 
-		gl.uniform4fv(shaderProgram.uColorAttribute, [1.0, lon%2, 1.0, 1.0]);
+		gl.uniform4fv(shaderProgram.uColorAttribute, [1.0, 0, 1.0, 1.0]);
 		setMatrixUniforms();
-		gl.bindBuffer(gl.ARRAY_BUFFER, circVertexPositionBuffer)
-		gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, circVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+		
 		gl.drawArrays(gl.LINE_LOOP, 0, circVertexPositionBuffer.numItems);
 
 		mvPopMatrix();
@@ -347,21 +462,89 @@ function drawEarthWF(pos,radius)
 	} 
 	mvPopMatrix();
 
-	for( lat = 0; lat < 90; lat ++) {
+	for( lat = -90; lat < 90; lat ++) {
 		mvPushMatrix();
 		var sz = radius*Math.cos(lat*Math.PI/180);
 
 		mat4.translate(mvMatrix, [pos.x, pos.y, pos.z+radius*Math.sin(lat*Math.PI/180)]);
 		mat4.scale(mvMatrix,[sz,sz,sz]);
 
-		gl.uniform4fv(shaderProgram.uColorAttribute, [1.0, 1.0, lat%2, 1.0]);
+		gl.uniform4fv(shaderProgram.uColorAttribute, [1.0, 1.0, 0, 1.0]);
 		setMatrixUniforms();
-		gl.bindBuffer(gl.ARRAY_BUFFER, circVertexPositionBuffer)
-		gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, circVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 		gl.drawArrays(gl.LINE_LOOP, 0, circVertexPositionBuffer.numItems);		
 		mvPopMatrix();
 	}
 	
+}
+
+function drawMap(depth) 
+{
+		
+	var depth = 6731-depth;
+	p1 = sphere2Cart(65.5,-18.52,depth);
+	p2 = sphere2Cart(63.8,-18.55,depth);
+	p3 = sphere2Cart(65.45 ,-14.8,depth);
+	p4 = sphere2Cart(63.75,-15.08,depth);
+	vertices = [
+	     p3.x,  p3.y,  p3.z,
+         p4.x,  p4.y,  p4.z,
+         p1.x,  p1.y,  p1.z,
+         p2.x,  p2.y,  p2.z
+	];
+
+	gl.bindBuffer(gl.ARRAY_BUFFER,mapVertexPositionBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices),gl.STATIC_DRAW);
+	mapVertexPositionBuffer.itemSize = 3;
+	mapVertexPositionBuffer.numItems = 4;
+
+	mvPushMatrix();
+	//mat4.translate(mvMatrix, [center.x, center.y, center.z]);
+	//mat4.scale(mvMatrix,[100,100,100]);
+	setMatrixUniforms();
+
+	gl.uniform4fv(shaderProgram.uColorAttribute, [1.0, 1.0, 1.0, 1.0]);
+	gl.bindBuffer(gl.ARRAY_BUFFER, mapVertexPositionBuffer)
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, mapVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, mapNormalVectorBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, mapNormalVectorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, mapVertexTextureCoordBuffer);
+    gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, mapVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+	gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, mapTexture);
+
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, mapVertexPositionBuffer.numItems);		
+		
+	mvPopMatrix();	
+}
+
+function handleLoadedTexture(texture) {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+
+//gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR); //gl.NEAREST is also allowed, instead of gl.LINEAR, as neither mipmap.
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE); //Prevents s-coordinate wrapping (repeating).
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE); //Prevents t-coordinate wrapping (repeating).
+
+    gl.bindTexture(gl.TEXTURE_2D, null);
+
+  }
+
+var mapTexture;
+function initTexture() {
+	mapTexture = gl.createTexture();
+	mapTexture.image = new Image();
+	mapTexture.image.onload = function() {
+	  handleLoadedTexture(mapTexture)
+	}
+
+	mapTexture.image.src = "map.png";
 }
 
 
